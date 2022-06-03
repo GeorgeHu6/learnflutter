2396a4e489c132ebebfd9d69dbf372b9c23753ac
GeorgeHu <dhxxhch@126.com>
2022-06-03 11:45:35 +0800

    create project

---
+++ lib/main.dart
@@ -0,0 +1,359 @@
+import 'package:flukit/flukit.dart';
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+import 'package:flutter/rendering.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // root widget
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: const HomePageList(),
+    );
+  }
+}
+
+class HomePageList extends StatelessWidget {
+  const HomePageList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(padding: const EdgeInsets.all(10), children: <Widget>[
+      ListTile(
+        title: const Text("AfterLayout"),
+        trailing: const Icon(Icons.featured_play_list_outlined),
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const AfterLayoutPage(),
+            ),
+          );
+        },
+      ),
+      ListTile(
+        title: const Text("LazyList"),
+        trailing: const Icon(Icons.alarm_add_outlined),
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const LazyListPage(),
+            ),
+          );
+        },
+      )
+    ]);
+  }
+}
+
+class LazyListPage extends StatelessWidget {
+  const LazyListPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('LazyList'),
+      ),
+      body: InfiniteListView(),
+    );
+  }
+}
+
+class InfiniteListView extends StatefulWidget {
+  @override
+  _InfiniteListViewState createState() => _InfiniteListViewState();
+}
+
+class _InfiniteListViewState extends State<InfiniteListView> {
+  static const loadingTag = "##loading##"; //表尾标记
+  var _words = <String>[loadingTag];
+
+  @override
+  void initState() {
+    super.initState();
+    _retrieveData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      itemCount: _words.length,
+      itemBuilder: (context, index) {
+        //如果到了表尾
+        if (_words[index] == loadingTag) {
+          //不足50条，继续获取数据
+          if (_words.length - 1 < 50) {
+            //获取数据
+            _retrieveData();
+            //加载时显示loading
+            return Container(
+              padding: const EdgeInsets.all(16.0),
+              alignment: Alignment.center,
+              child: const SizedBox(
+                width: 24.0,
+                height: 24.0,
+                child: CircularProgressIndicator(strokeWidth: 2.0),
+              ),
+            );
+          } else {
+            //已经加载了50条数据，不再获取数据。
+            return Container(
+              alignment: Alignment.center,
+              padding: const EdgeInsets.all(16.0),
+              child: const Text(
+                "没有更多了",
+                style: TextStyle(color: Colors.grey),
+              ),
+            );
+          }
+        }
+        //显示单词列表项
+        return ListTile(title: Text(_words[index]));
+      },
+      separatorBuilder: (context, index) {
+        print("Separator added at $index");
+        return const Divider(height: .0);
+        },
+    );
+  }
+
+  void _retrieveData() {
+    Future.delayed(Duration(seconds: 1)).then((e) {
+      setState(() {
+        //重新构建列表
+        _words.insertAll(
+          _words.length - 1,
+          //每次生成20个单词
+          generateWordPairs().take(20).map((e) => e.asPascalCase).toList(),
+        );
+      });
+    });
+  }
+}
+
+class AfterLayoutPage extends StatelessWidget {
+  const AfterLayoutPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('AfterLayout'),
+      ),
+      body: const AfterLayoutRoute(),
+    );
+  }
+}
+
+class AfterLayoutRoute extends StatefulWidget {
+  const AfterLayoutRoute({Key? key}) : super(key: key);
+
+  @override
+  _AfterLayoutRouteState createState() => _AfterLayoutRouteState();
+}
+
+class _AfterLayoutRouteState extends State<AfterLayoutRoute> {
+  String _text = 'flutter 实战 ';
+  Size _size = Size.zero;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Builder(
+            builder: (context) {
+              return GestureDetector(
+                child: const Text(
+                  'Text1: 点我获取我的大小',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.blue),
+                ),
+                onTap: () => print('Text1: ${context.size}'),
+              );
+            },
+          ),
+        ),
+        AfterLayout(
+          callback: (RenderAfterLayout ral) {
+            print('Text2: ${ral.size}, ${ral.offset}');
+          },
+          child: const Text('Text2: flutter@wendux'),
+        ),
+        Builder(builder: (context) {
+          return Container(
+            color: Colors.grey.shade200,
+            alignment: Alignment.center,
+            width: 100,
+            height: 100,
+            child: AfterLayout(
+              callback: (RenderAfterLayout ral) {
+                Offset offset = ral.localToGlobal(
+                  Offset.zero,
+                  ancestor: context.findRenderObject(),
+                );
+                print('A 在 Container 中占用的空间范围为：${offset & ral.size}');
+              },
+              child: const Text('A'),
+            ),
+          );
+        }),
+        const Divider(),
+        AfterLayout(
+          child: Text(_text),
+          callback: (RenderAfterLayout value) {
+            setState(() {
+              //更新尺寸信息
+              _size = value.size;
+            });
+          },
+        ),
+        //显示上面 Text 的尺寸
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8.0),
+          child: Text(
+            'Text size: $_size ',
+            style: const TextStyle(color: Colors.blue),
+          ),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            setState(() {
+              _text += 'flutter 实战 ';
+            });
+          },
+          child: const Text('追加字符串'),
+        ),
+      ],
+    );
+  }
+}
+
+/*
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  // homepage
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+*/
+
+/*
+  @override
+  Widget build(BuildContext context) {
+    // Rerun the build method for every setState calling.
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center includes a single child which will be in the middle.
+        child: Column(
+          // Column takes a list of child which will be show vertically.
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+  */
+  /*
+  Widget redBox = const DecoratedBox(
+    decoration: BoxDecoration(color: Colors.red),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        actions: const <Widget>[
+          UnconstrainedBox(
+            child: SizedBox(
+              width: 20,
+              height: 20,
+              child: CircularProgressIndicator(
+                strokeWidth: 3,
+                valueColor: AlwaysStoppedAnimation(Colors.white70),
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: Builder(builder: (context) {
+        return Container(
+          color: Colors.grey.shade200,
+          alignment: Alignment.center,
+          width: 100,
+          height: 100,
+          child: AfterLayout(
+            callback: (RenderAfterLayout ral) {
+              Offset offset = ral.localToGlobal(
+                Offset.zero,
+                // 传一个父级元素
+                ancestor: context.findRenderObject(),
+              );
+              print('A 在 Container 中占用的空间范围为：${offset & ral.size}');
+            },
+            child: Text('A'),
+          ),
+        );
+      }),
+    );
+  }*/
+//}
